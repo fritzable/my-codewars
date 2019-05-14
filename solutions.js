27ef002db2cac78ead9f7e1d3e5a122201407043
fritzable <fritzzillad@aol.com>
2019-05-14 14:28:44 -0400

    add another kata solution

--- solutions.js
+++ solutions.js
@@ -86,3 +86,41 @@ function titleCase (title, minorWords) {
   const titleCased = newArr.join(' ')
   return titleCased
 }
+
+// https://www.codewars.com/kata/format-a-string-of-names-like-bart-lisa-and-maggie/train/javascript
+//
+// Given: an array containing hashes of names
+//
+// Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+//
+// Example:
+//
+// list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// // returns 'Bart, Lisa & Maggie'
+//
+// list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// // returns 'Bart & Lisa'
+//
+// list([ {name: 'Bart'} ])
+// // returns 'Bart'
+//
+// list([])
+// // returns ''
+// Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
+
+function list (names) {
+  const nameArr = []
+  if (names.length > 1) {
+    for (let i = 0; i < (names.length - 2); i++) {
+      nameArr.push(`${names[i].name}, `)
+    }
+    nameArr.push(`${names[(names.length - 2)].name} & `)
+    nameArr.push(`${names[(names.length - 1)].name}`)
+  } else if (names.length > 0) {
+    return (`${names[0].name}`)
+  } else {
+    return ''
+  }
+  const str = nameArr.join('')
+  return str
+}
