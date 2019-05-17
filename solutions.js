4544c6a0f515759f0269cea01dec5e06e34fdb7d
fritzable <fritzzillad@aol.com>
2019-05-17 17:01:10 -0400

    add kata solution

--- solutions.js
+++ solutions.js
@@ -124,3 +124,35 @@ function list (names) {
   const str = nameArr.join('')
   return str
 }
+
+// https://www.codewars.com/kata/string-incrementer
+// Your job is to write a function which increments a string, to create a new string.
+//
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples:
+//
+// foo -> foo1
+//
+// foobar23 -> foobar24
+//
+// foo0042 -> foo0043
+//
+// foo9 -> foo10
+//
+// foo099 -> foo100
+//
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+function incrementString (str) {
+  const pattern = /(\D*)(\d*)/
+  const stringList = str.split(pattern)
+  if (stringList.length === 0) {
+    return '1'
+  } else {
+    const digits = (Number(stringList[2]) + 1)
+    const number = digits.toString().padStart(stringList[2].length, 0)
+    const incrementedString = (stringList[1] + number)
+    return incrementedString
+  }
+}
