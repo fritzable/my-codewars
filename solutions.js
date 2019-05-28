0b8824134b589aded82a4de87478ee41d6859b31
fritzable <fritzzillad@aol.com>
2019-05-28 08:51:44 -0400

    Add kata solution

--- solutions.js
+++ solutions.js
@@ -234,3 +234,35 @@ function alphabetPosition (text) {
   const joined = nums.join(' ')
   return joined
 }
+
+// https://www.codewars.com/kata/data-reverse/train/javascript
+// A stream of data is received and needs to be reversed.
+//
+// Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+//
+// 11111111  00000000  00001111  10101010
+//  (byte1)   (byte2)   (byte3)   (byte4)
+// should become:
+//
+// 10101010  00001111  00000000  11111111
+//  (byte4)   (byte3)   (byte2)   (byte1)
+// The total number of bits will always be a multiple of 8.
+//
+// The data is given in an array as such:
+//
+// [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+function dataReverse(data) {
+  const chunks = []
+  // split into 8 bit segments with slice
+  for (let i = 0; i < data.length; i += 8) {
+    const temp = data.slice(i, i + 8)
+    chunks.push(temp)
+  }
+  // reverse order of segments
+  const reversed = chunks.reverse()
+  // join with reduce and concat
+  const reducer = (acc, curr) => acc.concat(curr)
+  const newArr = reversed.reduce(reducer)
+  return newArr
+}
