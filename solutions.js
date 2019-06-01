67769bb91bee1ba2c2d37b96275ca762a9e3e9ea
fritzable <fritzzillad@aol.com>
2019-06-01 18:26:59 -0400

    Recursive snail
    -close, come back to snail

--- solutions.js
+++ solutions.js
@@ -266,3 +266,59 @@ function dataReverse(data) {
   const newArr = reversed.reduce(reducer)
   return newArr
 }
+
+// https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
+// Snail Sort
+// Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+//
+// array = [[1,2,3],
+//          [4,5,6],
+//          [7,8,9]]
+// snail(array) #=> [1,2,3,6,9,8,7,4,5]
+// For better understanding, please follow the numbers of the next array consecutively:
+//
+// array = [[1,2,3],
+//          [8,9,4],
+//          [7,6,5]]
+// snail(array) #=> [1,2,3,4,5,6,7,8,9]
+//
+// NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+//
+// NOTE 2: The 0x0 (empty matrix) is represented as [[]]
+
+snail = function (array) {
+  const arr = []
+  const n = array.length
+  const pattern = function (x) {
+
+    console.log('x is ', x)
+  // 0th array lists normally (0th-nth element)
+    x.shift().map(e => arr.push(e))
+    console.log('x is ', x)
+    console.log('arr is ', arr)
+    // 1st - nth-1 xs list last element
+    x.map(e => arr.push(e.pop()))
+    console.log('x is ', x)
+    console.log('arr is ', arr)
+    // last x list in reverse
+    x.pop().reverse().map(e => arr.push(e))
+    console.log('x is ', x)
+    console.log('arr is ', arr)
+    // nth-1 - 2nd xs list 1st element
+    x.reverse().map(e => arr.push(e.shift()))
+    console.log('x is ', x)
+    console.log('arr is ', arr)
+    x.reverse().pop().map(e => arr.push(e))
+    console.log('x is ', array)
+    console.log('arr is ', arr)
+    if (typeof x !== 'undefined' && x.length > 0) {
+  pattern(x)
+
+  } else {
+  return arr
+  }
+  }
+  // pattern begins again with 1st array as 0th array
+ pattern(array)
+
+}
